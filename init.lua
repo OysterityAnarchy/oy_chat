be2034af463d9a2eec3c9bb0ad0757343fd34067
cora <coradelamouche@gmx.ch>
2021-07-29 13:11:53 +0200

    let antispam handle /me etc. if present

--- init.lua
+++ init.lua
@@ -38,25 +38,27 @@ function minetest.send_leave_message(player_name, timed_out)
 	end
 end
 
-minetest.register_chatcommand("me", {
-	params = "<action>",
-	description = "Show chat action (e.g., '/me orders a pizza' displays '<player name> orders a pizza')",
-	privs = {shout = true},
-	func = function(name, param)
-		if param:find("<") or param:find(">") then
-			param = minetest.strip_colors(param)
-		end
-		minetest.chat_send_all(" " .. minetest.colorize("#B0B0B0", name .. " " .. param))
-		return true
-	end,
-})
+if not clam_antispam then
+	minetest.register_chatcommand("me", {
+		params = "<action>",
+		description = "Show chat action (e.g., '/me orders a pizza' displays '<player name> orders a pizza')",
+		privs = {shout = true},
+		func = function(name, param)
+			if param:find("<") or param:find(">") then
+				param = minetest.strip_colors(param)
+			end
+			minetest.chat_send_all(" " .. minetest.colorize("#B0B0B0", name .. " " .. param))
+			return true
+		end,
+	})
 
-minetest.register_chatcommand("greentext", {
-	params = "<action>",
-	description = "Sends a message in greentext",
-	privs = {shout = true},
-	func = function(name, param)
-		minetest.chat_send_all(minetest.colorize("#789922", " <" .. name .. ">: >" .. param))
-		return true
-	end,
-})
+	minetest.register_chatcommand("greentext", {
+		params = "<action>",
+		description = "Sends a message in greentext",
+		privs = {shout = true},
+		func = function(name, param)
+			minetest.chat_send_all(minetest.colorize("#789922", " <" .. name .. ">: >" .. param))
+			return true
+		end,
+	})
+end
